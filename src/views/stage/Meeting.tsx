c6b43d517e789c759741e63ea14846ed5da3339a
Udara J(SiniSTeR) <67888873+udaraJ08@users.noreply.github.com>
2023-10-12 14:42:02 +0530

    timestamp

--- src/views/stage/Meeting.tsx
+++ src/views/stage/Meeting.tsx
@@ -16,19 +16,25 @@ const Meeting = () => {
 
     const decoder = new TextDecoder();
 
-    room?.on(RoomEvent.DataReceived, (payload, participant, kind ) => {
+    useEffect(() => {
+        room?.on(RoomEvent.DataReceived, (payload, participant, kind ) => {
 
-        const strData = decoder.decode(payload);
-        const msg = JSON.parse(strData);
+            const strData = decoder.decode(payload);
+            const msg = JSON.parse(strData);
 
-        console.log('reached: RoomEvent.DataReceived')
-        console.log('kind: ', kind)
-        console.log('payload: ', payload)
-        console.log('strData: ', strData)
-        console.log('msg: ', msg)
-        console.log('+++++++++++++++++++++++++++++++')
-        console.log('')
-    })
+            const currentDate = new Date();
+            const unixTimestamp = Math.floor(currentDate.getTime() / 1000);
+            console.log('Unix Timestamp:', unixTimestamp);
+
+            console.log('reached: RoomEvent.DataReceived')
+            console.log('kind: ', kind)
+            console.log('payload: ', payload)
+            console.log('strData: ', strData)
+            console.log('msg: ', msg)
+            console.log('+++++++++++++++++++++++++++++++')
+            console.log('')
+        })
+    }, [])
 
     useEffect(() => {
         room?.localParticipant?.setCameraEnabled(false)
